5b94e3861610c500fbd358688e440d22af2b2025
NicolasB2 <nicolas.biojo@correo.icesi.edu.co>
2020-04-09 20:52:30 -0500

    Add style example

---
+++ 5_EstilosAndroid/app/src/main/java/com/example/estilosandroid/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.estilosandroid;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(this,new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.INTERNET,
+        },11);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode ==11){
+
+        }
+    }
+}
